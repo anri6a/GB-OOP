9fe4ea1eb118095d51aca3129196d2bfc717b164
Andrei Shyrakou <anri6a@gmail.com>
2023-10-02 22:48:21 +0300

    try with //todo comments

--- src/seminar01/MainCat.java
+++ src/seminar01/MainCat.java
@@ -4,7 +4,7 @@ public class MainCat {
     public static void main(String[] args) {
         Cat barsik = new Cat("barsik", 12, "white");
         barsik.setAge(32);
-        System.out.println("barsik = " + barsik);
+        System.out.println("barsik = " + barsik); //todo change cat name
     }
 
 }
